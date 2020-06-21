1a372a8a4ff0ca8c3006b1990ccc92e218fd8dae
urielCh <uriel.chemouni@gmail.com>
2020-06-21 05:41:16 +0300

    fill example

--- examples/fill-with-color.js
+++ examples/fill-with-color.js
@@ -21,7 +21,7 @@ class Example {
         // are "rgb", "rbg", "grb", "gbr", "bgr", "brg"
         // and "rgbw", "rbgw", "grbw", "grbw", "gbrw", "brgw", "bgrw" for SK6812 STRIP
         // Default is "rgb".
-        this.config.strip = 'grb';
+        this.config.strip = 'grbw';
 
         // Configure ws281x
         ws281x.configure(this.config);
@@ -33,16 +33,50 @@ class Example {
         const pixels = new Uint32Array(this.config.leds);
 
         // Create a fill color with red/green/blue.
-        const red = 255, green = 0, blue = 0;
-        const color = (red << 16) | (green << 8)| blue;
-
+        let red = 255, green = 0, blue = 0;
+        let color = (red << 16) | (green << 8) | blue;
+	console.log('Should Be RED');
         for (let i = 0; i < this.config.leds; i++)
             pixels[i] = color;
-
         // Render to strip
         ws281x.render(pixels);
+	ws281x.sleep(1000);
+
+
+        // Create a fill color with red/green/blue.
+        red = 0, green = 255, blue = 0;
+        color = (red << 16) | (green << 8) | blue;
+        console.log('Should Be Green');
+        for (let i = 0; i < this.config.leds; i++)
+            pixels[i] = color;
+        // Render to strip
+        ws281x.render(pixels);
+        ws281x.sleep(1000);
+
+
+        // Create a fill color with red/green/blue.
+        red = 0, green = 0, blue = 255;
+        color = (red << 16) | (green << 8) | blue;
+        console.log('Should Be Blue');
+        for (let i = 0; i < this.config.leds; i++)
+            pixels[i] = color;
+        // Render to strip
+        ws281x.render(pixels);
+        ws281x.sleep(1000);
+
+
+        // Create a fill color with red/green/blue.
+        red = 0, green = 0, blue = 0;
+	let white = 255;
+        color = (white << 24) | (red << 16) | (green << 8) | blue;
+        console.log('Should Be white');
+        for (let i = 0; i < this.config.leds; i++)
+            pixels[i] = color;
+        // Render to strip
+        ws281x.render(pixels);
+        ws281x.sleep(1000);
+
     }
-    
 };
 
 var example = new Example();
